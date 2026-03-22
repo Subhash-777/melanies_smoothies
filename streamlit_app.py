7b7441b280b720a20087cde375fdafe42e7e10f2
Subhash R <subhashravichandran7432@gmail.com>
2026-03-22 15:19:15 +0530

    Refactor ingredients string handling and API request
    Refactor ingredients string creation and add API call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,11 @@ ingredients_list = st.multiselect(
 if ingredients_list:
 
     # Convert Row → string
-    ingredients_string = ', '.join(ingredients_list)
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ''
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Submit button
     if st.button('Submit Order'):
